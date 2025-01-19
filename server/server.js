b8afd293d6d4f0f862f1f2bed3544f9547ccbdc7
Rohit sharma <rohitsharmatech9@gmail.com>
2025-01-19 12:20:54 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: "http://localhost:1234", 
+  origin: "https://qa-1.onrender.com/", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true,
